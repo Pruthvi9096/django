0605b5ca61c2fb3388fd9ba7c8f28c6ecec34edd
pruthvi <pruthvib@meditab.com>
2020-07-13 20:09:46 +0530

    small changes in api

--- crm/account/api/urls.py
+++ crm/account/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import customerView,customerCreateView,game
+from .views import customerView,customerCreateView,game,getUpdateDeleteCustomer
 
 urlpatterns = [
     path('customers/',customerView,name='customers'),
     path('customers/create/',customerCreateView,name='customer-create'),
+    path('customer/<int:pk>/',getUpdateDeleteCustomer),
     path('game/',game,name='game')
 ]
--- crm/account/api/views.py
+++ crm/account/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from django.http import HttpResponse,JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 @api_view(['GET'])
 def customerView(request):
@@ -26,6 +26,23 @@ def customerCreateView(request):
             serializer.save()
             return JsonResponse(serializer.data,status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def getUpdateDeleteCustomer(request,pk):
+    customer = Customer.objects.get(pk=pk)
+    if request.method == 'GET':
+        serializer = CustomerSerializer(customer,many=False)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    if request.method == 'PUT':
+        serializer = CustomerSerializer(instance=customer,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=status.HTTP_200_OK)
+        return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        customer.delete()
+        return JsonResponse({'message':'Customer was deleted successfully'},status=status.HTTP_204_NO_CONTENT)
+
 
 def game(request):
     return render(request,'fourdots.html',{})
