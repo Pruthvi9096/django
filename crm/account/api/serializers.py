3f117258c2d394154a455463e7aedfe882b01458
pruthvi <pruthvib@meditab.com>
2020-07-20 09:55:44 +0530

    small change

--- crm/account/api/serializers.py
+++ crm/account/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from account.models import Customer,Product,Order
+from account.models import Customer,Product,Order,Category,Tag
 from rest_framework.serializers import SerializerMethodField
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -7,7 +7,19 @@ class CustomerSerializer(serializers.ModelSerializer):
         model = Customer
         fields = '__all__'
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(required=False, read_only=True)
+    tags = TagSerializer(many=True, required=False, read_only=True)
     class Meta:
         model = Product
         fields = '__all__'
