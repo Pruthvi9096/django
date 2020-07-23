7c90c456f904564e56ca7d10688e6a1fd0216757
pruthvi <pruthvib@meditab.com>
2020-07-23 08:50:58 +0530

    url issue solved

--- twitter/twitter/urls.py
+++ twitter/twitter/urls.py
@@ -22,6 +22,7 @@ from ajax_select import urls as ajax_select_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('twitter/',include('twitterapp.urls')),
+    path('djrichtextfield/', include('djrichtextfield.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS)
