bbb183bc06cfc8a63d215280b5efe86807c5269c
pruthvi <pruthvib@meditab.com>
2020-10-06 13:52:59 +0530

    auth module with profile , login and register added

--- sale_proposal/auth_user/urls.py
+++ sale_proposal/auth_user/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/',views.registerView,name='register')
+    path('register/', views.registerView, name='register'),
+    path('login/', views.loginView, name='login'),
+    path('logout/', views.logoutView, name='logout')
 ]
--- sale_proposal/auth_user/views.py
+++ sale_proposal/auth_user/views.py
@@ -26,3 +26,20 @@ def registerView(request):
                 login(request, user)
                 return redirect('/')
     return render(request, 'frontend/register.html', {'form': form})
+
+def loginView(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username,password=password)
+        if user and user.is_active:
+            login(request,user)
+            return redirect('/')
+        else:
+            messages.error(request,"Invalid Username or Password!")
+    return render(request,'frontend/login.html',{'form':form})
+
+def logoutView(request):
+    logout(request)
+    return redirect('login')
